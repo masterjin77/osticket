a9a90370892876c131566ddea5b977bd43d43266
JEON SANGJIN <46582430+masterjin77@users.noreply.github.com>
2021-07-23 11:30:15 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','user24mysql.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@user24mysql');
 define('DBPASS','demo@pass123');
 
 # Table prefix
